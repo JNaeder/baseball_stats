3fde60f1c0c276fe9811a12c856598f38414a3f3
JNaeder324 <J.Naeder324@gmail.com>
2025-12-29 18:32:15 -0500

    Changed a few things

--- stats.py
+++ stats.py
@@ -11,22 +11,23 @@ class Stats:
         resp = requests.get(self._base_url + endpoint, params=params)
         if resp.status_code != 200:
             return
-        
+
         return resp.json()
 
-    def get_player_stats(self, year=2025):
+    def get_player_stats(self, year=2025, sportId=1):
         endpoint = "/api/v1/stats"
         params = {
+            "sportId": sportId,
             "stats": "season",
             "group": "hitting",
             "season": year,
             "playerPool": "all",
-            "limit": 1000
+            "limit": 1000,
         }
         res = self.get_data(endpoint=endpoint, params=params)
         data = res.get("stats", [])[0].get("splits", [])
         arr = []
-        
+
         for d in data:
             stat = d.get("stat")
             team = d.get("team")
@@ -51,29 +52,33 @@ class Stats:
                 "SF": stat.get("sacFlies"),
             }
             arr.append(new_data)
-        
+
         return pd.DataFrame(arr)
-    
+
     def get_team_stats(self, year=2025):
         leagues = [103, 104]
         all_standings = []
         endpoint = "/api/v1/standings"
 
         for league_id in leagues:
-            res = self.get_data(endpoint=endpoint, params={"leagueId": league_id, "season": year})
+            res = self.get_data(
+                endpoint=endpoint, params={"leagueId": league_id, "season": year}
+            )
             records = res.get("records", [])
             for record in records:
                 for team in record.get("teamRecords", []):
                     team_info = team.get("team", {})
-                    all_standings.append({
-                        "Id": team_info.get("id"),
-                        "Name": team_info.get("name"),
-                        "Season": team.get("season"),
-                        "G": team.get("gamesPlayed"),
-                        "W": team.get("wins"),
-                        "L": team.get("losses"),
-                        "RS": team.get("runsScored"),
-                        "RA": team.get("runsAllowed"),
-                        "Clinched": bool(team.get("clinchIndicator"))
-                    })
+                    all_standings.append(
+                        {
+                            "Id": team_info.get("id"),
+                            "Name": team_info.get("name"),
+                            "Season": team.get("season"),
+                            "G": team.get("gamesPlayed"),
+                            "W": team.get("wins"),
+                            "L": team.get("losses"),
+                            "RS": team.get("runsScored"),
+                            "RA": team.get("runsAllowed"),
+                            "Clinched": bool(team.get("clinchIndicator")),
+                        }
+                    )
         return pd.DataFrame(all_standings)
